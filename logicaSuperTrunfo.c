6b23933eed37281afcf73a6e90bde1b940e111d0
Renan F <167368572+Zerogallo@users.noreply.github.com>
2025-03-26 17:11:43 +0000

    trabalho aprimorador

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -127,69 +127,107 @@ int main() {
 
     int opcao, pais1, pais2, cidade1, cidade2, atributo;
 
-    // Menu interativo
-    do {
-        
-        printf("\nMenu de Comparacao de Cartas:\n");
-        printf("\n");
-        printf("1. Comparar duas Cartas\n");
-        printf("2. Sair\n");
-        printf("\n");
-        printf("Escolha uma opcao: ");
-        scanf("%d", &opcao);
-        printf("\n");
+// Menu interativo
+do {
     
-        if (opcao == 1) {
-            // Escolher as cidades para comparação
-            printf("Escolha duas Cartas para comparar:\n");
-            // Exibir países e suas cidades
-            for (int i = 0; i < NUM_ESTADOS; i++) {
-                printf("\nPais %d: %s\n\n", i + 1, estados[i].nomePais);
-                for (int j = 0; j < NUM_CIDADES; j++) {
-                    // Gerar o código da carta, que é composto pelo estado (A, B, C, ...) e número da cidade (1 a 4)
-                    printf("  (Carta: %c%02d) Cidade %d: %s ", 
-                            'A' + i,  // 'A' + i gera a letra correspondente ao estado
-                            j + 1,  // j + 1 gera o número da cidade (1 a 4)
-                            j + 1, 
-                            cidades[i][j].nome);
-                            
-                }
-                printf("\n");
+    printf("\nMenu de Comparacao de Cartas:\n");
+    printf("\n");
+    printf("1. Comparar duas Cartas\n");
+    printf("2. Sair\n");
+    printf("\n");
+    printf("Escolha uma opcao: ");
+    scanf("%d", &opcao);
+    printf("\n");
+
+    if (opcao == 1) {
+        // Escolher as cidades para comparação
+        printf("Escolha duas Cartas para comparar:\n");
+        // Exibir países e suas cidades
+        for (int i = 0; i < NUM_ESTADOS; i++) {
+            printf("\nPais %d: %s\n\n", i + 1, estados[i].nomePais);
+            for (int j = 0; j < NUM_CIDADES; j++) {
+                // Gerar o código da carta, que é composto pelo estado (A, B, C, ...) e número da cidade (1 a 4)
+                printf("  (Carta: %c%02d) Cidade %d: %s ", 
+                        'A' + i,  // 'A' + i gera a letra correspondente ao estado
+                        j + 1,  // j + 1 gera o número da cidade (1 a 4)
+                        j + 1, 
+                        cidades[i][j].nome);
+                        
             }
             printf("\n");
-    
-            // Primeira cidade - Escolher país e cidade
-            printf("\nPrimeira Carta - Digite o numero do pais (1 a 8): ");
+        }
+        printf("\n");
+
+        // Primeira cidade - Escolher país e cidade
+        do {
+            printf("Primeira Carta - Digite o numero do pais (1 a 8): ");
             scanf("%d", &pais1);
-            pais1--;  // Ajusta para o índice 0
-            printf("\n");
+    
+            // Verifica se o número está dentro do intervalo válido
+            if (pais1 < 1 || pais1 > 8) {
+                printf("Numero invalido! Por favor, digite um numero entre 1 e 8.\n");
+            }
+    
+        } while (pais1 < 1 || pais1 > 8);  // Repete até o número ser válido
+        pais1--;  // Ajusta para o índice 0
+        printf("\n");
+
+       
+        do {
             printf("Escolha a cidade do pais %s (1 a 4): ", estados[pais1].nomePais);
             scanf("%d", &cidade1);
-            cidade1--;  // Ajusta para o índice 0
-            printf("\n");
-            printf("Escolha a cidade %s  ",cidades[pais1][cidade1].nome);
-            printf("\n");
     
-            // Segunda cidade - Escolher país e cidade
-            printf("\nSegunda Carta - Digite o numero do pais (1 a 8): ");
+            // Verifica se o número está dentro do intervalo válido
+            if (cidade1 < 1 || cidade1 > 4) {
+                printf("Numero invalido! Por favor, Escolha a cidade do pais %s (1 a 4): \n", estados[pais1].nomePais);
+            }
+    
+        } while (cidade1 < 1 || cidade1 > 4);  // Repete até o número ser válido
+        cidade1--;  // Ajusta para o índice 0
+        printf("Escolha a cidade %s  ",cidades[pais1][cidade1].nome);
+
+        printf("\n");
+
+        // Segunda cidade - Escolher país e cidade
+      
+        do {
+            printf("Segunda Carta - Digite o numero do pais (1 a 8): ");
             scanf("%d", &pais2);
-            pais2--;  // Ajusta para o índice 0
-            printf("\n");
-            printf("Escolha a cidade do pais %s (1 a 4): ", estados[pais2].nomePais);
+    
+            // Verifica se o número está dentro do intervalo válido
+            if (pais2 < 1 || pais2 > 8) {
+                printf("Numero invalido! Por favor, digite um numero entre 1 e 8.\n");
+            }
+    
+        } while (pais2 < 1 || pais2 > 8);  // Repete até o número ser válido
+        pais2--;  // Ajusta para o índice 0
+        printf("\n");
+
+       
+        do {
+            printf("Escolha a cidade do pais %s (1 a 4): ", estados[pais1].nomePais);
             scanf("%d", &cidade2);
-            cidade2--;  // Ajusta para o índice 
-            printf("\n");
-            printf("Escolha a cidade %s  ", cidades[pais2][cidade2].nome);
-            printf("\n");
-            
     
-            printf("\n   %s, %s   &   %s, %s   \n", 
-                estados[pais1].nomePais, cidades[pais1][cidade1].nome, 
-                estados[pais2].nomePais, cidades[pais2][cidade2].nome);
+            // Verifica se o número está dentro do intervalo válido
+            if (cidade2 < 1 || cidade2 > 4) {
+                printf("Numero invalido! Por favor, Escolha a cidade do pais %s (1 a 4): \n", estados[pais2].nomePais);
+            }
+    
+        } while (cidade2 < 1 || cidade2 > 4);  // Repete até o número ser válido
+        cidade2--;  // Ajusta para o índice 
+        printf("Escolha a cidade %s  ", cidades[pais2][cidade2].nome);
+        printf("\n");
         
-            printf("\n");
-    
-            // Escolher o atributo para comparação
+
+        printf("   %s, %s   &   %s, %s   \n", 
+            estados[pais1].nomePais, cidades[pais1][cidade1].nome, 
+            estados[pais2].nomePais, cidades[pais2][cidade2].nome);
+
+        printf("\n");
+
+        // Escolher o atributo para comparação
+        do{
+
             printf("Escolha o atributo para comparacao:\n");
             printf("\n");
             printf("1. Populacao\n");
@@ -200,12 +238,20 @@ int main() {
             printf("Escolha um atributo: ");
             scanf("%d", &atributo);
             printf("\n");
-    
+
+            if (atributo < 1 || atributo > 4) {
+                printf("Numero invalido! Por favor, digite um numero entre 1 e 4.\n");
+            }
             // Comparar as cartas
             compararCartas(cidades[pais1][cidade1], cidades[pais2][cidade2], atributo);
-        }
-    } while (opcao != 2);
-    
-    return 0;
+        }while (opcao != 4);
+
     }
-    
+
+    if (opcao < 1 || opcao > 2) {
+        printf("Numero invalido! Por favor, digite um numero entre 1 e 2.\n");
+    }
+} while (opcao != 2);
+
+return 0;
+}
